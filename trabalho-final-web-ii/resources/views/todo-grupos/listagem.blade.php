c13ded703ea67ada03d368a348efb804f9f325b4
Djeferson Preis <djefersonpreis@gmail.com>
2021-06-18 12:58:46 -0300

    Grupos - Desenvolvimento inicial da listagem de Grupos
    Realizado criação visual da tabela de listagem de grupos

--- trabalho-final-web-ii/resources/views/todo-grupos/listagem.blade.php
+++ trabalho-final-web-ii/resources/views/todo-grupos/listagem.blade.php
@@ -5,9 +5,31 @@
 @stop
 
 @section('conteudo')
-    Grupos de To-dos
+        <h1 class="text-center">Grupos de To-dos </h1>
+        <button onclick="#" class="btn btn-outline-success align-right">Novo Grupo</button>
     <hr>
-    @foreach ($grupos as $g)
-        {{$g->name}}<br>
-    @endforeach
+    <table class="table table-striped table-dark">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrição</th>
+                <th scope="col">Data de Criação</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($grupos as $g)
+                <tr class="align-middle">
+                    <th scope="row">{{$g->id}}</th>
+                    <td>{{$g->name}}</td>
+                    <td>{{$g->description}}</td>
+                    <td>{{$g->created_at}}</td>
+                    <td>
+                        <button onclick="#" class="btn btn-outline-info">Editar</button>
+                        <button onclick="#" class="btn btn-outline-danger">Remover</button>
+                    </td>
+            @endforeach
+        </tbody>
+    </table>
 @stop
